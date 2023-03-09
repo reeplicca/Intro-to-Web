0e49231dae5eaee4c3e3d4553f96f3fce3671ac3
reeplicca <bekaidar4ik@mail.ru>
2023-03-09 14:50:08 +0600

    first commit

--- CHAPTER 8/Sprint Task 3/script.js
+++ CHAPTER 8/Sprint Task 3/script.js
@@ -1,23 +1,22 @@
 changeMoney.addEventListener('mouseout', changeCurrency);
+summaDeneg.addEventListener('keyup', changeCurrency);
 
 function changeCurrency(){
     let money = summaDeneg.value;
-    let currency = changeMoney.value
-    console.log(currency)
+    let currency = changeMoney.value;
 
     if (currency == "KZT") {
         document.getElementById('tradeKZT').textContent = '-'
         document.getElementById('tradeUSD').textContent = (money / 429).toFixed(2)
-        document.getElementById('tradeEUR').textContent = (money / 509).toFixed(2)
+        document.getElementById('tradeEUR').textContent = (money / 507).toFixed(2)
         
     } else if (currency == "USD") {
-        document.getElementById('tradeKZT').textContent = (money * 429).toFixed(2)
+        document.getElementById('tradeKZT').textContent = (money * 431).toFixed(2)
         document.getElementById('tradeUSD').textContent = '-'
-        document.getElementById('tradeEUR').textContent = (money * 429 / 507).toFixed(2)
+        document.getElementById('tradeEUR').textContent = (money * 431 / 507).toFixed(2)
     } else {
-        document.getElementById('tradeKZT').textContent = (money * 507).toFixed(2)
-        document.getElementById('tradeUSD').textContent = (money * 507 / 429).toFixed(2)
+        document.getElementById('tradeKZT').textContent = (money * 509).toFixed(2)
+        document.getElementById('tradeUSD').textContent = (money * 509 / 431).toFixed(2)
         document.getElementById('tradeEUR').textContent = '-'
     }
-
 }
